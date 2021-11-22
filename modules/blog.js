5fc4b906f776c6f3a3705d7be734a9ca1404ebf6
Abood-hass <60661627+Abood-hass@users.noreply.github.com>
2021-11-22 02:33:12 +0200

    no env

--- modules/blog.js
+++ modules/blog.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 require('dotenv').config({ path: './config.env' })
 
-mongoose.connect(process.env.dbConnection);
+mongoose.connect("mongodb+srv://abood:abood@cluster0.bqusa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"|| process.env.dbConnection);
 
 const blogSchema = new Schema({
     title:  String, // String is shorthand for {type: String}
